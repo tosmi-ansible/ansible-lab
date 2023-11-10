3703008f9a09c0dd77d0bc3889072c5632814e11
Toni Schmidbauer <toni@stderr.at>
2023-11-10 17:44:51 +0100

    start playing around with ansible lsp

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;; ((nil
+;;   . ((eglot-workspace-configuration
+;;       . (:yaml (:schemas (:kubernetes "/deployment.yaml")
+;; 			 )))
+;;      (eval . (when (string-match "\\.yaml|\\.yml" (buffer-file-name))
+;; 	       (ansible))))))
